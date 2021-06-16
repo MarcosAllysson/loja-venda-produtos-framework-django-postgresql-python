73dc5bd916eec709554c97c15af49314c05354f0
MarcosAllysson <marcos.silva3@estudante.ifb.edu.br>
2021-06-15 21:54:06 -0300

    fix media files

--- mysqlserver_django_python/wsgi.py
+++ mysqlserver_django_python/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+# Cling exibe arquivos estáticos, MediaCling exibe arquivos de upload
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysqlserver_django_python.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
